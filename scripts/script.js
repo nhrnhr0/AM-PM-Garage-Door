c2fa98b5a607c3c7062f2ad4ba7d1a8e3803b8e6
nhrnhr0 <nhrnhr0@gmail.com>
2021-08-30 19:45:04 +0300

    t

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
 var counter = document.getElementById("counters");
 var counterHeight = counter.clientHeight;
-document.addEventListener('scroll', animateCountersListener);
+var redpen = document.getElementById("redpen");
+document.addEventListener('scroll', scroll_listener);
 // check if element is in view
-function inView() {
+function inView(el) {
   // get window height
   var windowHeight = window.innerHeight;
   // get number of pixels that the document is scrolled
@@ -11,7 +12,7 @@ function inView() {
   // get current scroll position (distance from the top of the page to the bottom of the current viewport)
   var scrollPosition = scrollY + windowHeight;
   // get element position (distance from the top of the page to the bottom of the element)
-  var elementPosition = counter.getBoundingClientRect().top + scrollY + counterHeight;
+  var elementPosition = el.getBoundingClientRect().top + scrollY + counterHeight;
   
   // is scroll position greater than element position? (is element in view?)
   if (scrollPosition > elementPosition) {
@@ -20,12 +21,39 @@ function inView() {
   
   return false;
 }
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
 
+  );
+}
+function scroll_listener() {
+  animateCountersListener();
+  animateRedPenListenr();
+}
+
+var is_redpen_active = false;
+function animateRedPenListenr() {
+  var is_in_view = isInViewport(redpen);
+  if(is_redpen_active == false && is_in_view) {
+    is_redpen_active =true;
+    setTimeout(()=>{
+      redpen.classList.add('active');
+    },1000);
+  }else if(is_in_view == false){
+    is_redpen_active =false;
+    redpen.classList.remove('active');
+  }
+}
 // animate counters when it is in view
 var is_animation_started = false;
 function animateCountersListener() {
   // is element in view?
-  if (inView()) {
+  if (inView(counter)) {
       // element is in view, add class to element
       counter.classList.add('animate');
       if(is_animation_started == false){
